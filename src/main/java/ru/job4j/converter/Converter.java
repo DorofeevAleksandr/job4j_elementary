84cf258622ec208445bb8c02148064d43d65a287
DorofeevAleksandr <alexander.dorofeew@gmail.com>
2022-02-01 21:19:12 +0300

    5. Конвертер валюты [#41583]

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -7,15 +7,14 @@ public class Converter {
     }
 
     public static float rubleToDollar(float value) {
-        float rsl = value / 60; /* формула перевода рублей в доллары. */
+        float rsl = value / 60;
         return rsl;
     }
 
     public static void main(String[] args) {
         float euro = Converter.rubleToEuro(140);
         float dollar = Converter.rubleToDollar(140);
-        System.out.println("140 rubles are " + euro + " euro.");
-        System.out.println("140 rubles are " + dollar + " dollar.");
-
+        System.out.println(" 140 rubles are " + euro + " euro.");
+        System.out.println(" 140 rubles are " + dollar + " dollar.");
     }
 }
