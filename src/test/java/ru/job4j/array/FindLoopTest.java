641a95a624a5c834c24dbda2b99084fec70c3536
DorofeevAleksandr <alexander.dorofeew@gmail.com>
2022-02-21 23:22:43 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

--- src/test/java/ru/job4j/array/FindLoopTest.java
+++ src/test/java/ru/job4j/array/FindLoopTest.java
@@ -34,10 +34,43 @@ public class FindLoopTest {
 
     @Test
     public void whenArrayHas2Then3() {
-            int[] data = {5, 4, 3, 2};
-            int el = 2;
-            int result = FindLoop.indexOf(data, el);
-            int expected = 3;
-            Assert.assertEquals(expected, result);
+        int[] data = {5, 4, 3, 2};
+        int el = 2;
+        int result = FindLoop.indexOf(data, el);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHas2Then3() {
+        int[] data = new int[]{5, 2, 10, 2, 4};
+        int el = 2;
+        int start = 2;
+        int finish = 4;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 3;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHas8Then9() {
+        int[] data = new int[]{5, 2, 10, 2, 4, 6, 12, 7, 10, 8, 12, 17};
+        int el = 8;
+        int start = 3;
+        int finish = 10;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = 9;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenDiapasonHasNot8ThenMinus1() {
+        int[] data = new int[]{5, 2, 10, 2, 4, 6, 12, 7, 10, 8, 12, 17};
+        int el = 8;
+        int start = 1;
+        int finish = 7;
+        int result = FindLoop.indexOf(data, el, start, finish);
+        int expected = -1;
+        Assert.assertEquals(expected, result);
     }
 }
