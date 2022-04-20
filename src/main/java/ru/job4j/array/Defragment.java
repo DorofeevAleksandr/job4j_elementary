bab5de6a3e32199b7b357e9b8c410c62e991b38d
DorofeevAleksandr <alexander.dorofeew@gmail.com>
2022-04-20 08:50:56 +0300

    6.8. Дефрагментация массива [#225]

---
+++ src/main/java/ru/job4j/array/Defragment.java
@@ -0,0 +1,27 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int nullIndex = index;
+                for (int i = nullIndex + 1; i < array.length; i++) {
+                    if (array[i] != null) {
+                        int notNUll = i;
+                        array[index] = array[i];
+                        array[i] = null;
+                        break;
+                    }
+                }
+            }
+        }
+        return array;
+    }
+
+    public static String[] swap(String[] array, int source, int dest) {
+        String time = array[source];
+        array[source] = array[dest];
+        array[dest] = time;
+        return array;
+    }
+}
